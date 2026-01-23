5fdef34cb04a676998ccde010f792a28a2c86fe1
danny <danny.brogno@gmail.com>
2026-01-23 13:17:59 +0100

    Tricsy addition

--- src/components/underHeaderAboutUs.js
+++ src/components/underHeaderAboutUs.js
@@ -1,42 +1,48 @@
 import React from 'react';
-import leaves1 from '../img/leaves1.png';
+import aboutPizza1 from '../img/about-image-1.jpg'; 
+import aboutPizza2 from '../img/about-image-2.jpg';
+
 
 export const UnderHeaderAboutUs = () => {
   
   return (
     <div class="about-us-container">
     
+      <div>
+        
+      </div>
+
       <div class="set">
-        <div><img src={leaves1} alt="" /></div>
-        <div><img src={leaves1} alt="" /></div>
-        <div><img src={leaves1} alt="" /></div>
-        <div><img src={leaves1} alt="" /></div>
-        <div><img src={leaves1} alt="" /></div>
-        <div><img src={leaves1} alt="" /></div>
-        <div><img src={leaves1} alt="" /></div>
-        <div><img src={leaves1} alt="" /></div>
+        <div><img src={aboutPizza1} alt="" /></div>
+        <div><img src={aboutPizza1} alt="" /></div>
+        <div><img src={aboutPizza2} alt="" /></div>
+        <div><img src={aboutPizza1} alt="" /></div>
+        <div><img src={aboutPizza1} alt="" /></div>
+        <div><img src={aboutPizza1} alt="" /></div>
+        <div><img src={aboutPizza1} alt="" /></div>
+        <div><img src={aboutPizza2} alt="" /></div>
       </div>
 
       <div class="set set2">
-        <div><img src={leaves1} alt="" /></div>
-        <div><img src={leaves1} alt="" /></div>
-        <div><img src={leaves1} alt="" /></div>
-        <div><img src={leaves1} alt="" /></div>
-        <div><img src={leaves1} alt="" /></div>
-        <div><img src={leaves1} alt="" /></div>
-        <div><img src={leaves1} alt="" /></div>
-        <div><img src={leaves1} alt="" /></div>
+        <div><img src={aboutPizza1} alt="" /></div>
+        <div><img src={aboutPizza1} alt="" /></div>
+        <div><img src={aboutPizza2} alt="" /></div>
+        <div><img src={aboutPizza1} alt="" /></div>
+        <div><img src={aboutPizza1} alt="" /></div>
+        <div><img src={aboutPizza1} alt="" /></div>
+        <div><img src={aboutPizza1} alt="" /></div>
+        <div><img src={aboutPizza2} alt="" /></div>
       </div>
 
       <div class="set set3">
-        <div><img src={leaves1} alt="" /></div>
-        <div><img src={leaves1} alt="" /></div>
-        <div><img src={leaves1} alt="" /></div>
-        <div><img src={leaves1} alt="" /></div>
-        <div><img src={leaves1} alt="" /></div>
-        <div><img src={leaves1} alt="" /></div>
-        <div><img src={leaves1} alt="" /></div>
-        <div><img src={leaves1} alt="" /></div>
+        <div><img src={aboutPizza1} alt="" /></div>
+        <div><img src={aboutPizza1} alt="" /></div>
+        <div><img src={aboutPizza2} alt="" /></div>
+        <div><img src={aboutPizza1} alt="" /></div>
+        <div><img src={aboutPizza1} alt="" /></div>
+        <div><img src={aboutPizza1} alt="" /></div>
+        <div><img src={aboutPizza1} alt="" /></div>
+        <div><img src={aboutPizza2} alt="" /></div>
       </div>
 
     </div>
