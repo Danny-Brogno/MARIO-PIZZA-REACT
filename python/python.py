ab2bbf0a6adb862da5a3a644a2ce9b2f0709137e
danny <danny.brogno@gmail.com>
2026-02-05 17:27:05 +0100

    python

--- python/python.py
+++ python/python.py
@@ -1,27 +1,32 @@
 # Mario Pizza - Backend Logic
 pizza_menu = [
-    {"name": "Margherita", "price": 10, "ingredients": ["tomato", "mozzarella", "basil"]},
-    {"name": "Pepperoni", "price": 12, "ingredients": ["tomato", "mozzarella", "pepperoni"]},
-    {"name": "Quattro Formaggi", "price": 15, "ingredients": ["mozzarella", "gorgonzola", "parmesan", "fontina"]},
-    {"name": "Hawaiian", "price": 13, "ingredients": ["tomato", "mozzarella", "ham", "pineapple"]}
+    {"name": "Margherita", "price": 5, "ingredients": ["tomato", "mozzarella", "basil"]},
+    {"name": "Pepperoni", "price": 7, "ingredients": ["tomato", "mozzarella", "pepperoni"]},
+    {"name": "Sausage & turnip greens", "price": 8, "ingredients": ["mozzarella", "Italian sausage", "turnip greens"]},
+    {"name": "Bufala mozzarella", "price": 9, "ingredients": ["tomato", "buffalo mozzarella", "extra virgin olive oil"]},
+    {"name": "Prosciutto crudo & mozzarella", "price": 10, "ingredients": ["mozzarella", "prosciutto crudo", "basil"]},
+    {"name": "Primavera", "price": 10, "ingredients": ["mozzarella", "cherry tomatoes", "rocket", "", "prosciutto crudo"]},
+    {"name": "Yellow tomatoes & burrata", "price": 8, "ingredients": ["yellow tomatoes", "burrata cheese", "basil"]},
+    {"name": "Sundried tomatoes & bresaola", "price": 9, "ingredients": ["mozzarella", "sundried tomatoes", "bresaola"]},
+    {"name": "Four cheeses", "price": 8, "ingredients": ["mozzarella", "gorgonzola", "parmiggiano reggiano", "pecorino"]}
 ]
-
-def calculate_total(order_list):
-    total = 0
-    for item in order_list:
-        total += item['price']
-    return total
 
 def display_welcome():
     welcome_msg = "Welcome to Mario Pizza Backend System"
-    print("*" * len(welcome_msg))
+    print("#" * len(welcome_msg))
     print(welcome_msg)
-    print("*" * len(welcome_msg))
+    print("#" * len(welcome_msg))
 
-# Simulating a large amount of data processing logic
+# --- EXECUTION ---
+display_welcome()
+
 for pizza in pizza_menu:
-    print(f"Checking inventory for: {pizza['name']}...")
-    for ingredient in pizza['ingredients']:
-        print(f" - Ingredient {ingredient}: OK")
+    print("---------------------------------------------------")
+    # Join ingredients list into a single string separated by commas for better reading
+    ingredients_str = ", ".join(pizza['ingredients'])
+    # We use f-strings to show the Name, Price, and the Ingredients list
+    print(f"NAME: {pizza['name']} -> PRICE: £{pizza['price']} -> INGREDIENTS: {ingredients_str}")
 
-print(f"System ready. Awaiting orders from React Frontend...")
+print("###################################################")
+print(f"System ready. Awaiting orders from React Frontend.")
+print("###################################################")
