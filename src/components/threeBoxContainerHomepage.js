05cd48476e2b391d628517245be8431b5436c8b1
danny <danny.brogno@gmail.com>
2026-02-10 19:39:39 +0100

    added image enlarger

--- src/components/threeBoxContainerHomepage.js
+++ src/components/threeBoxContainerHomepage.js
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import pizza1 from '../img/27.jpg';
 import pizza2 from '../img/31.jpg';
 import pizza3 from '../img/32.jpg';
 
 // --------------------------
 export const ThreeBoxContainerHomepage = () => {
+  
+  // State to track which image is enlarged
+  const [enlargedImg, setEnlargedImg] = useState(null);
+
+  // Function to open the overlay
+  const handleImageClick = (imgSrc) => {
+    setEnlargedImg(imgSrc);
+  };
+
+  // Function to close the overlay
+  const closeOverlay = () => {
+    setEnlargedImg(null);
+  };
+  
   return (
     <div className="three-box-container-homepage">
       <h1>BEST SELLERS</h1>
+      
+      {enlargedImg && (
+        <div 
+          onClick={closeOverlay}
+          style={{
+            background: `RGBA(0,0,0,.9) url(${enlargedImg}) no-repeat center`,
+            backgroundSize: 'contain',
+            width: '100%',
+            height: '100%',
+            position: 'fixed',
+            zIndex: '10000',
+            top: '0',
+            left: '0',
+            cursor: 'pointer'
+          }}
+        />
+      )}
+      
       <div className="top">
         <div>
-          <img src={pizza1} alt="image-here" />
+          <img src={pizza1} alt="image-here" onClick={() => handleImageClick(pizza1)}/>
           <h3>Primavera</h3>
           <p>
             Mozzarella, fresh rocket, prosciutto crudo di Parma and olive oil.
@@ -19,7 +51,7 @@ export const ThreeBoxContainerHomepage = () => {
         </div>
         
         <div>
-          <img src={pizza2} alt="image-here" />
+          <img src={pizza2} alt="image-here" onClick={() => handleImageClick(pizza2)}/>
           <h3>Saugage and turnip greens</h3>
           <p>
             Grilled italian salsiccia, savory cime di rapa and olive oil.
@@ -28,7 +60,7 @@ export const ThreeBoxContainerHomepage = () => {
         </div>
         
         <div>
-          <img src={pizza3} alt="image-here" />
+          <img src={pizza3} alt="image-here" onClick={() => handleImageClick(pizza3)}/>
           <h3>Mozzarella di bufala</h3>
           <p>
             Mozzarella, buffalo mozzarella, cherry tomatoes, basil and olive oil.
