4a4321993086b4bfaf553faf019f913d18685342
danny <danny.brogno@gmail.com>
2026-02-13 19:11:29 +0100

    css added

--- src/components/makeYourOwnPizza.js
+++ src/components/makeYourOwnPizza.js
@@ -4,47 +4,49 @@ import makeYourOwnPizza from '../img/14.jpg';
 export const MakeYourOwnPizza = () => {
   return (
     <section className="make-your-own-pizza">
-      <div>
+      <div className="outer-container">
         <h3> MAKE YOUR OWN PIZZA!</h3>
         <h5>Choose up to 5 from this list of ingredients:</h5>
-        <ul>
-          <li>Italian sausages</li>
+        <div className="inside-container">
+          <ul>
+            <li>Italian sausages</li>
 
-          <li>Turnip greens</li>
+            <li>Turnip greens</li>
 
-          <li>Mozzarella di bufala</li>
+            <li>Mozzarella di bufala</li>
 
-          <li>Burrata cheese</li>
+            <li>Burrata cheese</li>
 
-          <li>Blue cheese</li>
+            <li>Blue cheese</li>
 
-          <li>Emmental</li>
+            <li>Emmental</li>
 
-          <li>Sundried tomatoes</li>
+            <li>Sundried tomatoes</li>
 
-          <li>Bresaola in slices</li>
+            <li>Bresaola in slices</li>
 
-          <li>Prosciutto Crudo di Parma in slices</li>
+            <li>Prosciutto Crudo di Parma in slices</li>
 
-          <li>Fresh rocket</li>
+            <li>Fresh rocket</li>
 
-          <li>Pepperoni</li>
+            <li>Pepperoni</li>
 
-          <li>Yellow cherry moatoes</li>
+            <li>Yellow cherry moatoes</li>
 
-          <li>Porcini mushrooms</li>
+            <li>Porcini mushrooms</li>
 
-          <li>Champignon mushrooms</li>
+            <li>Champignon mushrooms</li>
 
-          <li>Black olives</li>
+            <li>Black olives</li>
 
-          <li>Green olives</li>
+            <li>Green olives</li>
 
-          <li>Cooked ham in slices</li>
+            <li>Cooked ham in slices</li>
 
-          <li>Artichokes</li>
-        </ul> 
-        <img src={makeYourOwnPizza} />
+            <li>Artichokes</li>
+          </ul> 
+          <img src={makeYourOwnPizza} alt="image-here"/>
+        </div>
       </div>
     </section>
   )
