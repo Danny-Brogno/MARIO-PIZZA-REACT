63e8e54518291b04c27d4120035697177e16a34e
danny <danny.brogno@gmail.com>
2026-03-16 13:04:46 +0100

    fragment added

--- src/components/testing.js
+++ src/components/testing.js
@@ -10,8 +10,12 @@ export const Testing = () => {
   }
   
   return(
-    <button onClick={toggleClick}>
-      {isClicked ? "THANKS FOR CLIKING!!!" : "CLICK HERE!"}
-    </button>
+    <>
+      <div>
+        <button onClick={toggleClick}>
+          {isClicked ? "THANKS FOR CLIKING!!!" : "CLICK HERE!"}
+        </button>
+      </div>
+    </>
   )
 }
